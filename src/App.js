e52baad8624f9f1b3d8de45cc38176dc5178bc7f
Ishaan <itaneja27@gmail.com>
2021-08-07 00:02:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  /**
+   * constructor
+   *
+   * @object  @props  parent props
+   * @object  @state  component state
+   */
+  constructor(props) {
+
+      super(props);
+
+      this.state = {
+          items: [],
+          isLoaded: false
+      }
+
+  }
+
+  /**
+   * componentDidMount
+   *
+   * Fetch json array of objects from given url and update state.
+   */
+  componentDidMount() {
+
+      fetch('https://restcountries.eu/rest/v2/region/asia')
+          .then(res => res.json())
+          .then(json => {
+              this.setState({
+                  items: json,
+              })
+          }).catch((err) => {
+              console.log(err);
+          });
+
+  }
+
+  render() {
+
+    const { items } = this.state;
+    function refreshPage(){ 
+        window.location.reload(); 
+    }
+      
+      return (
+            <div className="elements">
+            <p className="heading">Countries In Asia.</p>
+            <button type="button" class="btn btn-lg btn-outline-primary" onClick={ refreshPage }>Refresh</button>
+                <div className="country container-fluid">
+                {items.map(item => (
+                    <div key={item.name} className="countrybox ">
+                     
+                    <h3>{item.name} </h3>
+                    <img src={item.flag} alt={item.name} class="countryimg"></img>
+                     <div className="image_overlay image_overlay--primary">
+                        <div className="image_description">
+                        <p>Capital : {item.capital} </p>
+                        <p> Region : {item.region}</p>
+                        <p>Subregion : {item.subregion}</p> 
+                        <p>Language: {item.languages[0].name}</p> 
+                        <p>Population: {item.population}</p>
+                        <div>Borders To: {item.borders.join(', ')}</div>
+                     </div>   
+                    
+                    </div>
+                   
+                  </div>
+      ))}
+
+  </div>
+  </div>
+
+      );
 }
-
+}
 export default App;
